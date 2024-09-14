be634eb05785f6aeca0cf9ed55d4e8520f732216
Athmadip <bhattacharyaatma@gmail.com>
2024-09-14 13:52:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    listener:80,
+    server http://13.235.68.130:3000,
     proxy: {
       "/api":{
-        target: "http://localhost:5000",
+        target: "http://13.235.68.130:5000",
         changeOrigin: true
       }
     }
