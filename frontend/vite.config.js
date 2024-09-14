b90bb6232c532ba085855d8c2ca598c145c35395
Athmadip <bhattacharyaatma@gmail.com>
2024-09-14 17:00:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -13,4 +13,24 @@ export default defineConfig({
       }
     }
   }
+})*/
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '13.235.68.130', // Bind the server to the specific IP address
+    port: 3000, // Use port 3000
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true
+      }
+    }
+  }
 })
+
+
